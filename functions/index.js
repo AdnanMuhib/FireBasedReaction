cd975ddc238ea8fe335a7432626a358f44a9b329
Adnan <adnan.muhib@rocketmail.com>
2019-12-22 07:03:38 +0500

    use express for get and post requests of screams

--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,48 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
 
 admin.initializeApp();
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World");
-});
-
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore()
-    .collection('screams')
-    .get()
-    .then(data => {
-        let screams= [];
+// Get a list of screams
+app.get('/screams', (req, res) => {
+    admin
+      .firestore()
+      .collection("screams")
+      .get()
+      .then(data => {
+        let screams = [];
         data.forEach(doc => {
-            screams.push(doc.data())
+          screams.push(doc.data());
         });
         return res.json(screams);
-    })
-    .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
 })
 
-exports.createScreams = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST') {
-        return res.status(400).json({error:'Method Not Allowed'});
-    }
-
+// post a new scream to database
+app.post('/scream', (req, res) => {
     const newScream = {
-        userHandle: req.body.userHandle,
-        body: req.body.body,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    userHandle: req.body.userHandle,
+    body: req.body.body,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
     };
 
-    admin.firestore()
-    .collection('screams')
+    admin
+    .firestore()
+    .collection("screams")
     .add(newScream)
-    .then(doc =>  {
-        res.json({message: `document ${doc.id} created successfully`});
+    .then(doc => {
+        res.json({ message: `document ${doc.id} created successfully` });
         return res;
     })
     .catch(err => {
-        res.status(500).json({error: "something went wrong"});
+        res.status(500).json({ error: "something went wrong" });
         console.error(err);
         return res;
     });
-
 });
+
+// https://baseurl.com/api/
+exports.api = functions.https.onRequest(app);

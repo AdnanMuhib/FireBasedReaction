1f581bf49d4c7f21af538b0893868342d31ffb5c
Adnan <adnan.muhib@rocketmail.com>
2019-12-22 08:36:32 +0500

    add dummy db schema for reference purpose

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is scream body",
+      createdAt: "2019-12-22T03:33:48.995Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ]
+};
